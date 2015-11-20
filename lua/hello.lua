66e1a71a2e80762955a32ff661f15d134268847a
unknown <wenlinli@WENLINLI-PC.sohu-inc.com>
2015-11-20 17:09:36 +0800

    doing resty server

---
+++ lua/hello.lua
@@ -0,0 +1,19 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: wenlinli
+-- Date: 2015/11/20
+-- Time: 15:48
+-- To change this template use File | Settings | File Templates.
+--
+
+local redis = require "redis.lredis"
+local red = redis:new()
+
+local ok, err = red:set("dog", "an animal")
+if not ok then
+    ngx.say("failed to set dog: ", err)
+    return
+end
+
+ngx.say("set result: ", ok)
+
